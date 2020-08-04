c31d4a8c73c1b62204f58a6ecc73eba3c54cbedb
akshay-e-k <akulkarni@chatsoft.com>
2020-08-04 20:48:03 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+// $(window).scroll(function () {
+// var st = $(this).scrollTop();
+//     if (st < 20){
+//         $(".pre-header-row").show();
+//         $(".main-header").css({"position":"fixed"});
+//     } else {
+//         $(".pre-header-row").hide();
+//         $(".main-header").css({"position":"sticky"});
+//     }
+
+// });
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 20) {
+        $(".pre-header-row").hide();
+        $(".main-header").css({"position":"sticky", "top": "0", "background-color": "#fff"});
+    } else {
+        $(".pre-header-row").show();
+        $(".main-header").css({"position":"fixed", "top": "36px","width": "100%", "background-color": "transparent"});
+    }
+});
