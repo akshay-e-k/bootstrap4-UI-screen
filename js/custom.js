7ff56fe98b4f750a08959697edf8ccb282687b75
akshay-e-k <akulkarni@chatsoft.com>
2020-08-05 19:30:29 +0530

    made responsive ui

--- js/custom.js
+++ js/custom.js
@@ -10,11 +10,13 @@
 
 // });
 $(window).scroll(function() {
-    if ($(this).scrollTop() > 20) {
-        $(".pre-header-row").hide();
-        $(".main-header").css({"position":"sticky", "top": "0", "background-color": "#fff"});
-    } else {
-        $(".pre-header-row").show();
-        $(".main-header").css({"position":"fixed", "top": "36px","width": "100%", "background-color": "transparent"});
+    if($(this).width() >= 992) {
+        if ($(this).scrollTop() > 20) {
+            $(".pre-header-row").hide();
+            $(".main-header").css({"position":"sticky", "top": "0", "background-color": "#fff", "zIndex": "1"});
+        } else {
+            $(".pre-header-row").show();
+            $(".main-header").css({"position":"fixed", "top": "36px","width": "100%", "background-color": "transparent"});
+        }
     }
 });
